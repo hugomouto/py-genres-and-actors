265cd4e7447fe2f454ed754a0038a1228904c230
Hugo Mouto <hmoutol@gmail.com>
2026-09-05 19:52:14 -0300

    Solution 3

--- main.py
+++ main.py
@@ -26,10 +26,10 @@ def main() -> QuerySet:
 
     Genre.objects.filter(name="Dramma").update(name="Drama")
     Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
-    Actor.objects.filter(last_name="Kianu").update(last_name="Keanu")
+    Actor.objects.filter(first_name="Kianu").update(first_name="Keanu")
     Actor.objects.filter(last_name="Reaves").update(last_name="Keanu")
 
     Genre.objects.filter(name="Action").delete()
-    Actor.objects.filter(name="Scarlett").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
 
-    return Genre.objects.filter(last_name="smith").order_by("first_name")
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
